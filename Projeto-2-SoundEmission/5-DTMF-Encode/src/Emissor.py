785f01ce42e4302957e04e8b5d5f0bd14a00ec80
Leostayner <leostayner@gmail.com>
2017-09-15 15:42:13 -0300

    modulação inicial

---
+++ Projeto-2-SoundEmission/5-DTMF-Encode/src/Emissor.py
@@ -0,0 +1,13 @@
+import sounddevice as sd
+fs = 44100
+
+audio = sd.rec(int(duration*fs), fs, channels=1)
+sd.wait()
+
+y = audio[:,0]
+
+# reproduz o som
+sd.play(y, fs)
+
+# aguarda fim da reprodução
+sd.wait()
